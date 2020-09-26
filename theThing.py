ae4ab7d9340555888a9f105f10d54cba15a014ca
adg516 <arjun.grover1601@gmail.com>
2020-09-25 23:30:12 -0500

    "sqlalchemy + flask"

--- theThing.py
+++ theThing.py
@@ -1,3 +1,6 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Float, Integer
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,13 +9,20 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import orm
 import sqlalchemy as sa
 
-base = declarative_base()
-engine = sa.create_engine('postgres://team25:team25@localhost:5432/team25')
-base.metadata.bind = engine
-session = orm.scoped_session(orm.sessionmaker())(bind=engine)
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = '123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dvetallprjyrmp:cb27c642041e9ac18355edcb59d1964de01eaf391555042ed5df0b4f41c782c8@ec2-34-238-26-109.compute-1.amazonaws.com:5432/d6drjb8qk3cd2g'
+db = SQLAlchemy(app)
+
+#base = declarative_base()
+#engine = sa.create_engine('postgres://team25:team25@localhost:5432/team25')
+#engine = sa.create_engine('postgres://dvetallprjyrmp:cb27c642041e9ac18355edcb59d1964de01eaf391555042ed5df0b4f41c782c8@ec2-34-238-26-109.compute-1.amazonaws.com:5432/d6drjb8qk3cd2g')
+#base.metadata.bind = engine
+#session = orm.scoped_session(orm.sessionmaker())(bind=engine)
 
 
-class User(base):
+class User(db.Model):
     __tablename__ = 'users'
 
     email = Column(String)
@@ -21,7 +31,7 @@ class User(base):
     password = Column(String)
     name = Column(String)
 
-class userGoals(base):
+class UserGoals(db.Model):
     __tablename__ = 'userGoals'
 
     user_id = Column(String, primary_key=True)
@@ -30,21 +40,30 @@ class userGoals(base):
     goalAge = Column(Integer)
     goalYear = Column(Integer)
 
-class userContent(base):
+class UserContent(db.Model):
     __tablename__ = 'userContent'
 
     contentText = Column(String)
     user_id = Column(String, primary_key=True)
 
-class generatedTimeline(base):
+class GeneratedTimeline(db.Model):
     __tablename__ = 'generatedTimeline'
 
     user_id = Column(String, primary_key=True)
     time = Column(Float)
     savings = Column(Float)
 
+class Index(Resource):
+  def get(self):
+    print("HOME PAGE")
 
-newUser = User(email='test2@gmail.com', user_id='testUser2', typee='student2', password='password2', name = 'bob2')
-session.add(newUser)
-session.commit()
+api.add_resource(Index, '/')
+if __name__ == '__main__':
+    app.run(debug=True)
+    newUser = User(email='test3@gmail.com', user_id='testUser3', typee='student3', password='password3', name = 'bob3')
+    db.session.add(newUser)
+    db.session.commit()
+
+
+
 
